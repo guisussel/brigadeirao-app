c220aba8a73ff70f84d6ccfded2db3ff94ea2100
guilhermesussel <susselguilherme@gmail.com>
2024-07-12 10:14:51 +0100

    added progress indicator on default message screen loading

--- app/src/main/java/com/sussel/brigadeirao/view/DefaultMessageScreen.kt
+++ app/src/main/java/com/sussel/brigadeirao/view/DefaultMessageScreen.kt
@@ -1,14 +1,28 @@
 package com.sussel.brigadeirao.view
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -16,6 +30,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sussel.brigadeirao.R
+import com.sussel.brigadeirao.ui.theme.cor5
 
 @Composable
 fun DefaultMessageScreen(message: String, showError: Boolean) {
@@ -25,18 +40,26 @@ fun DefaultMessageScreen(message: String, showError: Boolean) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        if(showError) {
+        if (showError) {
             Image(
                 // TODO put paint resources in a ENUM?
                 painter = painterResource(R.drawable.error),
                 contentDescription = "error"
             )
         }
-        Text(text = message,
+        Text(
+            text = message,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp,
             modifier = Modifier.padding(16.dp),
             textAlign = TextAlign.Center,
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .requiredSize(100.dp)
+                .width(64.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
         )
     }
 }
